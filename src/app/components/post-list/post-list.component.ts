2efd99bd228a2ef29ebcffc48bbabf7070e91031
jgomesmv <jose.gomes@hibob.io>
2024-09-28 00:35:18 +0100

    improve post list component with inject function

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, Signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PostService } from '../../services/post.service';
-import { Post } from '../../types/models/post.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -13,12 +12,9 @@ import { toSignal } from '@angular/core/rxjs-interop';
   imports: [CommonModule, RouterModule],
 })
 export class PostListComponent {
-  posts: Signal<Post[]>;
+  postService = inject(PostService);
 
-  constructor(private postService: PostService) {
-    // Convert the Observable to a Signal
-    this.posts = toSignal(this.postService.getAll(), { initialValue: [] });
-  }
+  posts = toSignal(this.postService.getAll(), { initialValue: [] });
 
   shortenBody(body: string): string {
     return body.length > 100 ? body.slice(0, 100) + '...' : body;
